d4de607447183feca381afd33b3247b19c6e98c0
Reynel <reynelvillabonag@gmail.com>
2023-06-04 12:07:03 +0200

    function modified

--- src/components/AddButton.js
+++ src/components/AddButton.js
@@ -11,13 +11,18 @@ const AddNewRowForm = ({data}) => {
     image: '',
   };
 
-  const [formData, setFormData] = useState(data || initialFormData);
+  const [formData, setFormData] = useState(initialFormData);
   const [FormModified, setFormModified] = useState(false);
 
   useEffect(() => {
-  const isModified = Object.keys(formData).some((key) => formData[key] !== data[key]);
-  setFormModified(isModified);
+  const isSubstrateIdEqual = formData.substrate_id === data.substrate_id;
+  const isSubstrateIdEmpty = !formData.substrate_id;
+
+  const isAllFieldsEqual = Object.keys(formData).every((key) => formData[key] === data[key]);
+
+  setFormModified( isSubstrateIdEqual && !isAllFieldsEqual && !isSubstrateIdEmpty);
 }, [formData, data]);
+
 
 
 
@@ -27,8 +32,7 @@ const AddNewRowForm = ({data}) => {
 
   const handleClearForm = () => {
       setFormModified(false);
-
-  setFormData(initialFormData);
+      setFormData(initialFormData);
 };
 
   const submitChanges = () => {
@@ -55,32 +59,33 @@ const AddNewRowForm = ({data}) => {
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [name]: value,
-    }));
-  };
+    let parsedValue = value;
+
+  if (['substrate_id', 'growth_days', 'day_light_integral', 'cutting_height', 'yield'].includes(name)) {
+    parsedValue = parseFloat(value);
+    if (Number.isNaN(parsedValue)) {
+      parsedValue = '';
+    }
+  }
+
+  setFormData((prevFormData) => ({
+    ...prevFormData,
+    [name]: parsedValue,
+  }));
+};
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
 
-    const newRowData = {
-      substrate_id: parseInt(formData.substrate_id),
-      growth_days: parseFloat(formData.growth_days),
-      day_light_integral: parseFloat(formData.day_light_integral),
-      cutting_height: parseFloat(formData.cutting_height),
-      yield: parseFloat(formData.yield),
-      image: formData.image,
-    };
+    
 
-    addNewRow(newRowData)
+    addNewRow(formData)
       .then(() => {
         console.log('New row added successfully!');
         window.location.reload(); // Reload the app 
       })
       .catch((error) => {
         console.log('Error adding new row', error);
-        // Handle any errors that occur during insertion
       });
   };
 
@@ -148,11 +153,23 @@ const AddNewRowForm = ({data}) => {
       </div>
       <button type="submit">Add New Row</button>
       <button type="button" onClick={handleClearForm}>Clear Form</button>
-      {FormModified && formData.substrate_id === data.substrate_id &&  JSON.stringify(formData) !== JSON.stringify(initialFormData) && (
+      {FormModified && (
   <button type="button" onClick={submitChanges}>
     Update Form
   </button>
+  
 )}
+
+{/* <pre>
+  {Object.entries(formData).map(([key, value]) => (
+    <div key={key}>
+      {key}: {value} (Type: {typeof value})
+    </div>
+  ))}
+</pre> */}
+
+
+
 
 
     </form>
